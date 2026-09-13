7437f7ab866f404277c4317008006ae93158467d
markkashraf <mark2001rko2001@gmail.com>
2026-09-14 02:19:56 +0300

    initial commit

---
+++ src/app/features/category/category-list/category-list.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  imports: [RouterLink],
+  selector: 'app-category-list',
+  styleUrl: './category-list.css',
+  templateUrl: './category-list.html',
+})
+export class CategoryList {}
